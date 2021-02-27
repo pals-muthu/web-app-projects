c3c80d15366fec726915c01b0a90087afd8f97d0
pals-muthu <mpalaniapp96@gmail.com>
2021-02-27 06:40:13 -0500

    implemented load database and basic templating

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
+
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    // we're connected!
+    console.log("DB connected");
+});
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
---
+++ yelpCamp/seeds/index.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const { descriptors, places } = require('./seedHelpers');
+const cities = require('./cities');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    // we're connected!
+    console.log("DB connected");
+});
+
+const clearDb = async () => {
+    await Campground.deleteMany({});
+}
+
+const sampleData = array => array[Math.floor(Math.random() * array.length)]
+
+const populateData = async () => {
+    for (let i = 0; i < 100; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sampleData(descriptors)} ${sampleData(places)}`
+        })
+        await camp.save();
+        console.log(camp);
+    }
+}
+
+const seedDB = async () => {
+    // clear the existing contents before adding fresh ones.
+    await clearDb();
+    //create random Data
+    await populateData();
+    console.log("Db data dump complete.")
+
+}
+
+seedDB().then(() => {
+    db.close();
+})
