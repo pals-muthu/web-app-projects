b509b7cb2acc5fe95751648d8367dffa3b91fafc
pals-muthu <mpalaniappan96@gmail.com>
2021-03-29 13:18:45 -0400

    Deployement related changes

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -142,7 +142,7 @@ app.use((err, req, res, next) => {
 });
 
 // ------------------------------------------------------------------------------------
-
-app.listen('4000', () => {
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
     console.log("Listening on port 8000 on localhost and 12000 broadcast");
 });
