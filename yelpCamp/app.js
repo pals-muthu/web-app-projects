7842826543c673384a6657c819f35a3839e8ed08
pals-muthu <mpalaniappan96@gmail.com>
2021-04-13 13:16:40 -0400

    mongodb location update

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -10,8 +10,8 @@ if (process.env.NODE_ENV !== "production") {
 // ------------------------------------------------------------------------------------
 // MONGOOSE SPECIFICS
 
-// const db_url = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-const db_url = 'mongodb://localhost:27017/yelp-camp';
+const db_url = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+// const db_url = 'mongodb://localhost:27017/yelp-camp';
 console.log("db_url: ", db_url);
 const mongoose = require('mongoose');
 mongoose.connect(db_url,
