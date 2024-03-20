5816e5241349f2320c1067436cd55bdf29240d96
pal-e5 <palaniappan.muthukaruppan@e5.ai>
2024-03-20 18:31:37 +0530

    till input and output

--- angularLearner/src/app/base/component-one/component-one.component.ts
+++ angularLearner/src/app/base/component-one/component-one.component.ts
@@ -1,46 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
-export interface LearnerObject {
-  arr: PeriodicElement[]
-}
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LearnerObject, PeriodicElement } from '../../utils/types';
 
 @Component({
   selector: 'app-component-one',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './component-one.component.html',
   styleUrl: './component-one.component.css'
 })
 export class ComponentOneComponent {
 
-  incomingObject: LearnerObject = {
-    arr: ELEMENT_DATA,
+  @Input('elementData') ELEMENT_DATA: PeriodicElement[] = [];
+  @Output() counterUpdated = new EventEmitter<Number>();
 
+  incomingObject: LearnerObject = {
+    arr: [],
   };
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    if (changes['ELEMENT_DATA']) {
+      this.incomingObject.arr = this.ELEMENT_DATA;
+    }
+  }
+
+  counterName: number = 0;
 
   handleClickEvent (event: Event) {
     console.log('event target: ', event.target);
+  }
+
+  handleChangeEvent (event: Event) {
+    const value = Number.parseInt((event.target as HTMLInputElement).value);
+    console.log('event target: ', value);
+    this.counterUpdated.emit(value);
   }
 
 }
