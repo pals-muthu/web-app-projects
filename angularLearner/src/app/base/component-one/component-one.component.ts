872a6e37ac79ab2b66fc58ad00d705b0f56bae4d
pal-e5 <palaniappan.muthukaruppan@e5.ai>
2024-03-20 18:54:55 +0530

    view child and content child

--- angularLearner/src/app/base/component-one/component-one.component.ts
+++ angularLearner/src/app/base/component-one/component-one.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ContentChild, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { LearnerObject, PeriodicElement } from '../../utils/types';
 
@@ -10,10 +10,13 @@ import { LearnerObject, PeriodicElement } from '../../utils/types';
   templateUrl: './component-one.component.html',
   styleUrl: './component-one.component.css'
 })
-export class ComponentOneComponent {
+export class ComponentOneComponent implements OnChanges, AfterViewInit, AfterContentInit {
 
   @Input('elementData') ELEMENT_DATA: PeriodicElement[] = [];
   @Output() counterUpdated = new EventEmitter<Number>();
+  
+  @ViewChild('refOne') refOne: any;
+  @ContentChild('refBase') refBase: any;
 
   incomingObject: LearnerObject = {
     arr: [],
@@ -25,6 +28,18 @@ export class ComponentOneComponent {
     if (changes['ELEMENT_DATA']) {
       this.incomingObject.arr = this.ELEMENT_DATA;
     }
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log('loader values ngAfterViewInit: ', this.refOne, this.refBase)
+  }
+
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    console.log('loader values ngAfterContentInit: ', this.refOne, this.refBase)
   }
 
   counterName: number = 0;
