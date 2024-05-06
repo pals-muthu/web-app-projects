2fc2785f714a2d7d2bcbbaf51049e9b8a014ef28
pals-muthu <mpalaniappan96@gmail.com>
2024-05-06 07:49:15 +0530

    base framework

---
+++ angularExpenseTracker/src/app/components/expense-item/expense-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './expense-item.component.html',
+  styleUrl: './expense-item.component.css'
+})
+export class ExpenseItemComponent {
+
+}
