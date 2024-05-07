e24c5c274648f8c09b8509ce02cac75d02552f80
pal-e5 <palaniappan.muthukaruppan@e5.ai>
2024-05-07 17:20:51 +0530

    adding a form

--- angularExpenseTracker/src/app/components/add-expense/add-expense.component.ts
+++ angularExpenseTracker/src/app/components/add-expense/add-expense.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-expense',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, FormsModule],
   templateUrl: './add-expense.component.html',
   styleUrl: './add-expense.component.css'
 })
 export class AddExpenseComponent {
+
+  onSubmit(formRef: NgForm) {
+    console.log('formRef: ', formRef.value);
+  }
 
 }
