c2d23fd02435fab5e4f41d09155d1f3784f74292
pals-muthu <mpalaniappan96@gmail.com>
2024-05-07 09:13:42 +0530

    semi stable version minus active link

--- angularExpenseTracker/src/app/helpers/side-nav/side-nav.component.ts
+++ angularExpenseTracker/src/app/helpers/side-nav/side-nav.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from "@angular/common";
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import { config } from '../../utils/config';
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'side-nav',
   standalone: true,
-  imports: [MatIconModule, MatListModule, CommonModule],
+  imports: [MatIconModule, MatListModule, CommonModule, RouterModule],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.css'
 })
@@ -14,8 +16,5 @@ export class SideNavComponent {
   @Input() isExpanded: boolean;
   @Output() toggleMenu = new EventEmitter();
 
-  public routeLinks = [
-    { link: "about", name: "About", icon: "dashboard" },
-    { link: "locations", name: "Locations", icon: "account_balance" },
-  ];
+  public routeLinks = Object.values(config.CONSTANTS.ROUTES);
 }
---
+++ angularExpenseTracker/src/app/utils/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  CONSTANTS: {
+    ROUTES: {
+      HOME: { link: '', name: "Add Expense", icon: "playlist_add" },
+      VIEW: { link: 'view-expense', name: "View Expenses", icon: "view_timeline" }
+    }
+  }
+}
