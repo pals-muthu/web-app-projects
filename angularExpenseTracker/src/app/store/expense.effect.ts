425885bab88d841144e2568fcd9093288f48bade
pal-e5 <palaniappan.muthukaruppan@e5.ai>
2024-05-09 10:47:46 +0530

    minor improvements

--- angularExpenseTracker/src/app/store/expense.effect.ts
+++ angularExpenseTracker/src/app/store/expense.effect.ts
@@ -3,45 +3,24 @@ import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { ExpenseService } from "../services/expense.services";
 import { Observable, catchError, exhaustMap, finalize, map, of, tap } from "rxjs";
 import { Store } from "@ngrx/store";
+import { Loader } from "./general.store";
 
 @Injectable()
 export class ExpenseEffects {
-
+  private loader = new Loader(this.store);
   constructor (private actions: Actions, private expenseService: ExpenseService, private store: Store) {
   }
-
-  turnOnLoader () {
-    this.store.dispatch({ type: 'loader/on'});
-  }
-
-  turnOffLoader () {
-    this.store.dispatch({ type: 'loader/off'});
-  }
-
-  // processRequest (funcPipe: Observable<any>) {
-  //   this.turnOnLoader();
-  //   return exhaustMap(() => {
-  //     return funcPipe.pipe(
-  //       finalize(() => {
-  //         this.turnOffLoader();
-  //       })
-  //     )
-  //   })
-  // }
 
   fetchExpenses = createEffect(() =>
     this.actions.pipe(
       ofType('effect/expense/fetch'),
-      // this.processRequest(this.expenseService.getExpenses().pipe(
-      //   map(expenses => ({ type : 'expense/fetch', payload: expenses})),
-      //   catchError(() => of({ type : 'expense/error' })))),
       exhaustMap(() => {
-        this.turnOnLoader();
+        this.loader.turnOnLoader();
         return this.expenseService.getExpenses().pipe(
         map(expenses => ({ type : 'expense/fetch', payload: expenses})),
         catchError(() => of({ type : 'expense/error' })),
         finalize(() => {
-          this.turnOffLoader();
+          this.loader.turnOffLoader();
         })
       )
     })
@@ -51,12 +30,12 @@ export class ExpenseEffects {
   addExpense = createEffect(() => this.actions.pipe(
     ofType('effect/expense/add'),
     exhaustMap((action) => {
-      this.turnOnLoader();
+      this.loader.turnOnLoader();
       return this.expenseService.createExpenses(action['payload']).pipe(
       map(expense => ({ type : 'expense/add', payload: expense})),
       catchError(() => of({ type : 'expense/error' })),
       finalize(() => {
-        this.turnOffLoader();
+        this.loader.turnOffLoader();
       })
     )})
   ));
--- angularExpenseTracker/src/app/store/general.store.ts
+++ angularExpenseTracker/src/app/store/general.store.ts
@@ -1,4 +1,5 @@
-import { createAction, createReducer, on } from "@ngrx/store";
+import { Injectable } from "@angular/core";
+import { Store, createAction, createReducer, on } from "@ngrx/store";
 
 export const loaderOn = createAction('loader/on');
 export const loaderOff = createAction('loader/off');
@@ -15,3 +16,19 @@ export const generalReducer = createReducer(initialGeneralState,
   on(loaderOn, (state) => ({ ...state, loader: true })),
   on(loaderOff, (state) => ({ ...state, loader: false })),
 );
+
+@Injectable()
+export class Loader {
+
+  constructor (private store: Store) {
+  }
+
+  turnOnLoader () {
+    this.store.dispatch({ type: 'loader/on'});
+  }
+
+  turnOffLoader () {
+    this.store.dispatch({ type: 'loader/off'});
+  }
+
+}
