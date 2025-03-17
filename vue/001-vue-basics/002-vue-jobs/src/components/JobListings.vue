5bbd21da1897f1df147cfb0581edf61916a78e39
pal-freshworks <palaniappan.muthukaruppan@freshworks.com>
2025-03-17 21:51:40 +0530

    adding a spinner and job page

--- vue/001-vue-basics/002-vue-jobs/src/components/JobListings.vue
+++ vue/001-vue-basics/002-vue-jobs/src/components/JobListings.vue
@@ -2,6 +2,7 @@
 import { defineProps, onMounted, reactive } from 'vue'
 import { RouterLink } from 'vue-router'
 import JobListing from './JobListing.vue'
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 import axios from 'axios'
 
 defineProps({
@@ -36,8 +37,10 @@ onMounted(async () => {
   <section class="bg-green-50 px-4 py-10">
     <div class="container-xl lg:container m-auto">
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <!-- Job Listing 1 -->
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
           v-for="job of state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
