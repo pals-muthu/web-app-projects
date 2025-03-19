50fbb5410a282b9ebedda87f422c13e07645587a
pal-freshworks <palaniappan.muthukaruppan@freshworks.com>
2025-03-19 22:36:07 +0530

    add job and toast notifications

--- vue/001-vue-basics/002-vue-jobs/src/router/index.js
+++ vue/001-vue-basics/002-vue-jobs/src/router/index.js
@@ -3,6 +3,7 @@ import HomePage from '@/views/HomePage.vue'
 import JobsPage from '@/views/JobsPage.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import JobPage from '@/views/JobPage.vue'
+import AddJobView from '@/views/AddJobView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       path: '/jobs/:id',
       name: 'job',
       component: JobPage,
+    },
+    {
+      path: '/jobs/add',
+      name: 'Add Job',
+      component: AddJobView,
     },
     {
       path: '/:pathMatch(.*)*',
--- vue/001-vue-basics/002-vue-jobs/vite.config.js
+++ vue/001-vue-basics/002-vue-jobs/vite.config.js
@@ -8,6 +8,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5010',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
